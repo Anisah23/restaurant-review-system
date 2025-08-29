35586f86cc9d8873a765c452394b54d63b8088e3
Anisah <ibrahimanisah45@gmail.com>
2025-08-29 20:43:52 +0300

    migrations complete

--- migrations/versions/1fb0ea406863_initial_tables.py
+++ migrations/versions/1fb0ea406863_initial_tables.py
@@ -18,3 +18,36 @@ branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table('restaurants',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('name', sa.String(), nullable=True),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('reviews',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('star_rating', sa.Integer(), nullable=True),
+    sa.Column('comment', sa.String(), nullable=True),
+    sa.Column('restaurant_id', sa.Integer(), nullable=True),
+    sa.ForeignKeyConstraint(['restaurant_id'], ['restaurants.id'], ),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('menu_items',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('name', sa.String(), nullable=True),
+    sa.Column('price', sa.Float(), nullable=True),
+    sa.Column('description', sa.String(), nullable=True),
+    sa.Column('restaurant_id', sa.Integer(), nullable=True),
+    sa.ForeignKeyConstraint(['restaurant_id'], ['restaurants.id'], ),
+    sa.PrimaryKeyConstraint('id')
+    )
+   
+
+
+def downgrade() -> None:
+    """Downgrade schema."""   
+    op.drop_table('menu_items')
+    op.drop_table('reviews')
+    op.drop_table('restaurants')
+   
